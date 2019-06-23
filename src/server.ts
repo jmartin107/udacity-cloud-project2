2b968efe69e3d4a454739da66b7c6e3d0a6a0867
Jeff Martin <jeff_martin@archibus.com>
2019-06-23 09:17:25 -0400

    Improve imageFilter request error handling

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,25 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles, validateFileType} from './util/util';
+import express from "express";
+import bodyParser from "body-parser";
+import {
+  filterImageFromURL,
+  deleteLocalFiles,
+  validateFileType
+} from "./util/util";
 import * as path from "path";
 import asyncHandler from "express-async-handler";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -27,7 +27,7 @@ import asyncHandler from "express-async-handler";
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  //   the filtered image file
 
   /**************************************************************************** */
 
@@ -36,18 +36,21 @@ import asyncHandler from "express-async-handler";
     asyncHandler(async (req, res, next) => {
       let image_url = req.query.image_url;
 
+      // Check if the image_url query parameter exists
       if (!image_url) {
-        // const error = new Error("missing image_url");
-        // throw error;
+        res.status(400).send("Image URL is missing");
+        return next();
+      }
 
-        res.status(400).send('Image URL is missing')
-        next()
-        return
+      // Check if the URL file type is valid
+      if (!validateFileType(image_url)) {
+        res.status(400).send("Image Mime type is not valid")
+        return next();
       }
 
       try {
         let isValidURL = validateFileType(image_url);
-        console.log("isValidURL: ", isValidURL);
+        
         let imagePath = await filterImageFromURL(image_url);
         let absoluteImagePath = path.join(__dirname, "util", imagePath);
         res.status(200).sendFile(absoluteImagePath, function(err: any) {
@@ -63,17 +66,32 @@ import asyncHandler from "express-async-handler";
     })
   );
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
+
+  app.use(
+    (
+      err: { stack: any },
+      req: any,
+      res: { status: (arg0: number) => { send: (arg0: string) => void } },
+      next: any
+    ) => {
+      /* now here if your error in development then send stack trace to display whole error,
+    if it's in production then just send error message and log error with some library as winston etc.. */
+      // if(process.env.NODE_ENV === 'production') {
+      //  return res.status(500).send(`something wen't wrong`);
+      //}
+      res.status(500).send(`hey!! we caugth the error 👍👍, ${err.stack} `);
+    }
+  );
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
