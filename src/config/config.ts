48d2e599ea2fa4ae78f792ed6373662c3ee97124
Jeff Martin <jeff_martin@archibus.com>
2019-06-24 14:53:02 -0400

    Add JSON Web Token authorization

---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  jwt: {
+    username: process.env.JWT_USERNAME,
+    secret: process.env.JWT_SECRET
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import {
   filterImageFromURL,
@@ -7,6 +7,31 @@ import {
 } from "./util/util";
 import * as path from "path";
 import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import { NextFunction } from "connect";
+import { config } from "./config/config"
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (!req.headers || !req.headers.authorization) {
+    return res.status(401).send({ message: "No authorization headers." });
+  }
+
+  const token_bearer = req.headers.authorization.split(" ");
+  if (token_bearer.length != 2) {
+    return res.status(401).send({ message: "Malformed token." });
+  }
+
+  const token = token_bearer[1];
+
+  return jwt.verify(token, config.jwt.secret, err => {
+    if (err) {
+      return res
+        .status(500)
+        .send({ auth: false, message: "Failed to authenticate." });
+    }
+    return next();
+  });
+}
 
 (async () => {
   // Init the Express application
@@ -15,6 +40,9 @@ import asyncHandler from "express-async-handler";
   // Set the network port
   const port = process.env.PORT || 8082;
 
+  const token = jwt.sign(config.jwt.username, config.jwt.secret);
+
+  console.log("token: ", token);
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -33,6 +61,7 @@ import asyncHandler from "express-async-handler";
 
   app.get(
     "/filteredImage",
+    requireAuth,
     asyncHandler(async (req, res, next) => {
       let image_url = req.query.image_url;
 
@@ -44,13 +73,13 @@ import asyncHandler from "express-async-handler";
 
       // Check if the URL file type is valid
       if (!validateFileType(image_url)) {
-        res.status(400).send("Image Mime type is not valid")
+        res.status(400).send("Image Mime type is not valid");
         return next();
       }
 
       try {
         let isValidURL = validateFileType(image_url);
-        
+
         let imagePath = await filterImageFromURL(image_url);
         let absoluteImagePath = path.join(__dirname, "util", imagePath);
         res.status(200).sendFile(absoluteImagePath, function(err: any) {
@@ -65,7 +94,6 @@ import asyncHandler from "express-async-handler";
       }
     })
   );
-  //! END @TODO1
 
   // Root Endpoint
   // Displays a simple message to the user
@@ -73,21 +101,14 @@ import asyncHandler from "express-async-handler";
     res.send("try GET /filteredimage?image_url={{}}");
   });
 
-  app.use(
-    (
-      err: { stack: any },
-      req: any,
-      res: { status: (arg0: number) => { send: (arg0: string) => void } },
-      next: any
-    ) => {
-      /* now here if your error in development then send stack trace to display whole error,
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    /* now here if your error in development then send stack trace to display whole error,
     if it's in production then just send error message and log error with some library as winston etc.. */
-      // if(process.env.NODE_ENV === 'production') {
-      //  return res.status(500).send(`something wen't wrong`);
-      //}
-      res.status(500).send(`hey!! we caugth the error 👍👍, ${err.stack} `);
-    }
-  );
+    // if(process.env.NODE_ENV === 'production') {
+    //  return res.status(500).send(`something wen't wrong`);
+    //}
+    res.status(500).send(`hey!! we caugth the error 👍👍, ${err.stack} `);
+  });
 
   // Start the Server
   app.listen(port, () => {
